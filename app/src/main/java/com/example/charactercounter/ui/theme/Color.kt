6ed8df35280cc4090f602759893c62c9fc09526d
VicSchbt <victoire.schubert@gmail.com>
2025-03-31 22:07:28 +0200

    design system set up

--- app/src/main/java/com/example/charactercounter/ui/theme/Color.kt
+++ app/src/main/java/com/example/charactercounter/ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Neutral900 = Color(0xff12131a)
+val Neutral800 = Color(0xff21222c)
+val Neutral700 = Color(0xff2a2b37)
+val Neutral600 = Color(0xff404254)
+val Neutral200 = Color(0xffe4e4ef)
+val Neutral100 = Color(0xfff2f2f7)
+val Neutral0 = Color(0xffffffff)
+
+val Purple400 = Color(0xffd3a0fa)
+val Purple500 = Color(0xffc27cf8)
+
+val Yellow500 = Color(0xffff9f00)
+
+val Orange500 = Color(0xfffe8159)
+val Orange800 = Color(0xffda3701)
--- app/src/main/java/com/example/charactercounter/ui/theme/Type.kt
+++ app/src/main/java/com/example/charactercounter/ui/theme/Type.kt
@@ -2,9 +2,58 @@ package com.example.charactercounter.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.charactercounter.R
+
+val DMSans = FontFamily(
+    Font(
+        resId = R.font.dm_sans_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.dm_sans_semi_bold,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resId = R.font.dm_sans_bold,
+        weight = FontWeight.Bold
+    )
+)
+
+val textPreset1 = TextStyle(
+    fontFamily = DMSans,
+    fontWeight = FontWeight.Bold,
+    fontSize = 40.sp,
+    lineHeight = 40.sp,
+    letterSpacing = (-1).sp
+)
+
+val textPreset2 = TextStyle(
+    fontFamily = DMSans,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+    letterSpacing = (-1).sp
+)
+
+val textPreset3 = TextStyle(
+    fontFamily = DMSans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp,
+    lineHeight = 28.sp,
+    letterSpacing = (-0.6).sp
+)
+
+val textPreset4 = TextStyle(
+    fontFamily = DMSans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (-0.6).sp
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +64,4 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
