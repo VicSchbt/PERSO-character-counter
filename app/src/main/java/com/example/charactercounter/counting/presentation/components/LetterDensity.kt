eaac2b93bd707f908f76ac5080df86d6c43e62ae
VicSchbt <victoire.schubert@gmail.com>
2025-04-14 21:01:35 +0200

    feat: LetterDensity toggle

--- app/src/main/java/com/example/charactercounter/counting/presentation/components/LetterDensity.kt
+++ app/src/main/java/com/example/charactercounter/counting/presentation/components/LetterDensity.kt
@@ -7,13 +7,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import com.example.charactercounter.R
 import com.example.charactercounter.ui.theme.CharacterCounterTheme
 import com.example.charactercounter.ui.theme.Neutral100
+import com.example.charactercounter.ui.theme.Neutral900
 import com.example.charactercounter.ui.theme.Purple400
 
 @Composable
@@ -32,6 +43,9 @@ fun LetterDensity(
     val sortedLetterDensity: List<Map.Entry<Char, Int>> =
         letterDensity.entries.sortedByDescending { it.value }
 
+    var showAll by remember { mutableStateOf(false) }
+    val visibleItems = if (showAll) sortedLetterDensity else sortedLetterDensity.take(5)
+
     Column(
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(20.dp)
@@ -41,14 +55,31 @@ fun LetterDensity(
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
-            sortedLetterDensity.forEach {
+            visibleItems.forEach {
                 LetterDensityItem(
                     letter = it.key,
                     letterCount = it.value,
                     totalLetter = totalLetter
                 )
             }
+            if (sortedLetterDensity.size > 5) {
+                TextButton(
+                    onClick = { showAll = !showAll}
+                ) {
+                    Text(
+                        text = if (!showAll) stringResource(R.string.see_more) else stringResource(R.string.see_less),
+                        style = MaterialTheme.typography.labelSmall,
+                        color = Neutral900,
+                    )
+                    Icon(
+                        imageVector = if (!showAll) Icons.Outlined.KeyboardArrowDown else Icons.Outlined.KeyboardArrowUp,
+                        contentDescription = ""
+                    )
+                }
+            }
         }
+
+
     }
 
 
@@ -94,7 +125,7 @@ fun LetterDensityItem(
 private fun LetterDensityPreview() {
     CharacterCounterTheme {
         LetterDensity(
-            letterDensity = mapOf(Pair('e', 40), Pair('i', 29), Pair('t', 18)),
+            letterDensity = mapOf(Pair('e', 40), Pair('i', 29), Pair('t', 18), Pair('a', 40), Pair('j', 29), Pair('y', 18)),
             totalLetter = 189
         )
     }
